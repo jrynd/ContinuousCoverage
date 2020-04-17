aaac9d3cb9b6032df84f4dbfbd0b6108e7951214
jrynd <2bnp4gukr5@snkmail.com>
2020-04-17 19:51:14 -0400

    Create Main.scala

---
+++ Main.scala
@@ -0,0 +1,18 @@
+package com.jrynd.jobapplication.cigna.continuouscoverage
+
+object TestContinuousCoverage {
+  val coverages = List(
+    Cov(1, 20),
+    Cov(21, 30),
+    Cov(15, 25),
+    Cov(28, 40),
+    Cov(50, 60),
+    Cov(61, 200),
+  )
+ 
+  def main(args: Array[String]): Unit = {
+    val longestCoverage = getLongestCoverage(coverages)
+
+    println(s"For coverages $coverages\nlongest is $longestCoverage")
+  }
+}
